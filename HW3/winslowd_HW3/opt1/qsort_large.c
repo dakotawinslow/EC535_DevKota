7d8d58b72361913def0e111f8b3c7afee15c8b1c
winslowd <winslowd@bu.edu>
2025-04-17 10:25:28 -0400

    Added inclass 4-17

---
+++ HW3/winslowd_HW3/opt1/qsort_large.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define UNLIMIT
+#define MAXARRAY 60000 /* this number, if too large, will cause a seg. fault!! */
+#define LOOPS 100 /* loop ourselves for better timing data*/
+
+struct my3DVertexStruct {
+    int x, y, z;
+    double distance;
+};
+
+// int compare(const void *elem1, const void *elem2)
+// {
+//     /* D = [(x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2]^(1/2) */
+//     /* sort based on distances from the origin... */
+
+//     double distance1, distance2;
+
+//     distance1 = (*((struct my3DVertexStruct *)elem1)).distance;
+//     distance2 = (*((struct my3DVertexStruct *)elem2)).distance;
+
+//     return (distance1 > distance2) ? 1 : ((distance1 == distance2) ? 0 : -1);
+// }
+
+int compare(const void *a, const void *b) {
+    // Directly reference the distance field of the struct
+    // instead of using a pointer dereference
+    // This avoids the need for a temporary variable - winslowd
+    const struct my3DVertexStruct *v1 = (const struct my3DVertexStruct *)a;
+    const struct my3DVertexStruct *v2 = (const struct my3DVertexStruct *)b;
+
+    return (v1->distance > v2->distance) ? 1 : ((v1->distance == v2->distance) ? 0 : -1);
+}
+
+
+int
+main(int argc, char *argv[]) {
+    struct my3DVertexStruct array[MAXARRAY];
+    FILE *fp;
+    int i,count=0;
+    int x, y, z;
+
+    if (argc<2) {
+        fprintf(stderr,"Usage: qsort_large <file>\n");
+        exit(-1);
+    }
+    else {
+        fp = fopen(argv[1],"r");
+
+        while((fscanf(fp, "%d", &x) == 1) && (fscanf(fp, "%d", &y) == 1) && (fscanf(fp, "%d", &z) == 1) &&  (count < MAXARRAY)) {
+            array[count].x = x;
+            array[count].y = y;
+            array[count].z = z;
+            // array[count].distance = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+            // Avoid using pow and sqrt for performance
+            array[count].distance = x * x + y * y + z * z; // Store squared distance - winslowd
+            count++;
+        }
+    }
+    printf("\nSorting %d vectors based on distance from the origin.\n\n",count);
+    qsort(array,count,sizeof(struct my3DVertexStruct),compare);
+
+    for(i=0; i<count; i++)
+        printf("%d %d %d\n", array[i].x, array[i].y, array[i].z);
+    return 0;
+}
+
+// int main(int argc, char *argv[]) {
+//     int i;
+//     int result = 0;
+//     for (i = 0; i < LOOPS; i++) {
+//         result = mainloop(argc, argv);
+//     }
+//     return result;
+// }
